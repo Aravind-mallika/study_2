b60fc063ede419999e2c43f48428b3fbbf7c9ca9
Aravind <aravindmano9493@gmail.com>
2024-04-02 17:01:28 +0530

    initial commmit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Araavind");
+  res.send("Araa111111111111111111vind");
 });
 
 module.exports = router;
